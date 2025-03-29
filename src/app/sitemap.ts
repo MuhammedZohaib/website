3b8b4d72707844e3d947941c194a218055008d4d
Muhammad Zohaib <alizuhaib828@gmail.com>
2025-03-29 18:13:07 +0500

    fix: fix domain name in sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://yourwebsite.com";
+  const baseUrl = "https://zoha1b.vercel.app";
 
   // Define all your routes
   const routes = [
